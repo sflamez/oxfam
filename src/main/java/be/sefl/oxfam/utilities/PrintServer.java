18a3afb51fd05493398047b8ccd8683a4666dd46
Sem Flamez <Sem.Flamez@gmail.com>
2017-10-05 17:08:54 +0200

    Clean.

--- src/main/java/be/sefl/oxfam/utilities/PrintServer.java
+++ src/main/java/be/sefl/oxfam/utilities/PrintServer.java
@@ -6,23 +6,19 @@ import javax.swing.SwingWorker;
 
 public class PrintServer extends SwingWorker {
 
-    protected Object doInBackground() throws Exception {
-        try {
-            ServerSocket serverSocket = new ServerSocket(9999);
+	protected Object doInBackground() throws Exception {
+		try (ServerSocket serverSocket = new ServerSocket(9999)) {
+			while (true) {
+				(new PrinterThread(serverSocket.accept())).start();
+				Thread.sleep(1000L);
+			}
+		} catch (IOException e) {
+			System.err.println("Failed to open up server socket on port 9999");
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 
-            while(true) {
-                (new PrinterThread(serverSocket.accept())).start();
-                Thread.sleep(1000L);
-            }
-
-            return null;
-        } catch (IOException e) {
-            System.err.println("Failed to open up server socket on port 9999");
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
+		return null;
+	}
 }
