a5bce2f6c30fa39034840e29768b1ad4d9aea202
Sem Flamez <Sem.Flamez@smals.be>
2019-09-13 17:53:38 +0200

    Initial spring-boot setup.

---
+++ src/main/java/be/sefl/oxfam/cashregister/OxfamCashRegisterApplication.java
@@ -0,0 +1,13 @@
+package be.sefl.oxfam.cashregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxfamCashRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxfamCashRegisterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/sefl/oxfam/cashregister/OxfamCashRegisterApplicationTests.java
@@ -0,0 +1,16 @@
+package be.sefl.oxfam.cashregister;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OxfamCashRegisterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
